119ab4b451b98da412d9df8422303148e262fbb7
Danilo Manchon dos Santos <51140620+DaniloManchon@users.noreply.github.com>
2024-09-19 21:54:11 -0300

    Develop (#6)
    * add diagrama de arquitetura
    
    * update to java 21
    
    * add collection insomnia
    
    * add collection insomnia
    
    * add helm
    
    * add terraform
    
    * add terraform
    
    * add terraform
    
    * fix main.tf
    
    * fix main.tf
    
    * fix variables.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.techchallenge_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.techchallenge_cluster.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--output", "json"]
@@ -21,13 +21,17 @@ provider "helm" {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
 resource "helm_release" "app" {
   name             = var.app_name
-  repository       = file("${path.module}/helm/backend")
+  repository       = "${path.module}/helm/backend"
   chart            = var.app_name
   namespace        = var.app_name
   create_namespace = true
