afb4f87a813ed5fe5cdf3ed2586758022b4fdef9
Danilo Manchon <danilomanchon@gmail.com>
2024-09-19 21:43:33 -0300

    add terraform

--- infra/main.tf
+++ infra/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.cluster.cluster_name
+    host                   = aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
@@ -22,7 +22,7 @@ provider "helm" {
 }
 
 data "aws_eks_cluster" "cluster" {
-    name=var.cluster_name
+  name = var.cluster_name
 }
 
 
