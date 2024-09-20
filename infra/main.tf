37929c0c3dc7788eb94849559d43d7b30bc5a072
Danilo Manchon <danilomanchon@gmail.com>
2024-09-19 21:50:17 -0300

    fix main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--output", "json"]
